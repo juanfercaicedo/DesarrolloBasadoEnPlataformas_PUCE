83d15df6f99625415819a4eed32ed2c3e194d4de
Estudiante LTIC <Estudiante@ltic.local>
2026-09-25 12:01:17 -0500

    Explicación código

--- Clases/8_25SEP/taller/script.js
+++ Clases/8_25SEP/taller/script.js
@@ -6,7 +6,7 @@ class ServicioError extends Error {
 }
 
 function obtenerDatosUsuario() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => { // Un resultado que puede estar disponble al instante, en un futuro o nunca
     setTimeout(() => {
       const exito = Math.random() > 0.3;
       if (exito) {
@@ -14,14 +14,14 @@ function obtenerDatosUsuario() {
       } else {
         reject(new ServicioError("No se pudo conectar con el servicio"));
       }
-    }, 800);
+    }, 800); // establecemos un tiempo de espera de 800 milsegundos para que se complete el servicio
   });
 }
 
-const boton = document.getElementById("btnCargar");
+const boton = document.getElementById("btnCargar"); // Obtenemos el DOM por el ID
 const resultado = document.getElementById("resultado");
 
-boton.addEventListener("click", async () => {
+boton.addEventListener("click", async () => { // Cuando demos click en el botón mostrará un cargando
   resultado.textContent = "Cargando...";
   try {
     const datos = await obtenerDatosUsuario();
